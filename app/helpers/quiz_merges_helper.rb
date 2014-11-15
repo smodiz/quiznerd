3c8ec3478b2e8355d4ff5f0f1cf5b519c119b9ad
Sharon Modiz <sharonmodiz@yahoo.com>
2014-11-15 12:11:30 -0700

    Minor changes, including moving sort from QuizMerge to a view helper, changing a validation error to appear on the target instead of the source, and changes to increase code readability.

--- app/helpers/quiz_merges_helper.rb
+++ app/helpers/quiz_merges_helper.rb
@@ -2,7 +2,7 @@ module QuizMergesHelper
 
   def mergable_quizzes_select
     @quizzes ||= QuizMerge.mergable_quizzes_for(current_user).
-      map { |q| [q.name, q.id] }
+      map { |q| [q.name, q.id] }.sort
   end
 
 end
