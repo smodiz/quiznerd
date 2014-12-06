d3a32fd9088f8521e51f46e96fa3a45156ba595e
Sharon Modiz <sharonmodiz@yahoo.com>
2014-12-06 15:29:57 -0700

    Commenting out part of migration script due to heroku error.

--- db/migrate/20141205154211_add_question_count_to_quiz.rb
+++ db/migrate/20141205154211_add_question_count_to_quiz.rb
@@ -1,9 +1,9 @@
 class AddQuestionCountToQuiz < ActiveRecord::Migration
   def change
     add_column :quizzes, :questions_count, :integer, default: 0
-    Quiz.reset_column_information
-    Quiz.all.each do |quiz|
-      quiz.update_attribute :questions_count, quiz.questions.length
+    # Quiz.reset_column_information
+    # Quiz.all.each do |quiz|
+    #   quiz.update_attribute :questions_count, quiz.questions.length
     end
   end
 end
