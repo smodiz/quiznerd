fcf36dcd5974e5bf5c941ad54e0845b6bbf2a300
Sharon Modiz <sharonmodiz@yahoo.com>
2015-01-23 18:30:11 -0700

    Minor refactoring and cleanup of views related to Quiz

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,5 +1,18 @@
 module QuizzesHelper
 
-  
+  def new_quiz_button(min_quizzes_size: 1)
+    if @quizzes.size >= min_quizzes_size
+      link_to '<i class="glyphicon glyphicon-plus glyphicon-white"></i> New Quiz'.html_safe, 
+        new_quiz_path, class: 'btn btn-primary pull-right btn-sm' 
+    end
+  end
+
+  def publish_label
+    @quiz.published ? "Unpublish" : "Publish"
+  end
+
+  def show_published_link?
+    @quiz.published || (!@quiz.published && @quiz.can_publish?)
+  end
 
 end
--- app/views/quizzes/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@quizzes) do |quiz|
-  json.extract! quiz, :id, :name, :description, :author_id, :published, :category_id, :subject_id
-  json.url quiz_url(quiz, format: :json)
-end
