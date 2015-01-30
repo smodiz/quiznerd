538e5d9088d810df0dcf860125ce2441d99240d2
Sharon Modiz <sharonmodiz@yahoo.com>
2015-01-30 11:41:58 -0700

    Fix dynamic deleting of answers

--- app/assets/javascripts/question_answers.js
+++ app/assets/javascripts/question_answers.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
-  $('#question_question_type').on('click', set_true_false_defaults);
+  $('#question_question_type').on('click', setTrueFalseDefaults);
 
-  $('form').on('click','.destroy_checkbox',function() {
+  $('form').on('click','.destroy',function() {
       $(this).closest('.answer-row').hide();
   });
 
@@ -13,19 +13,21 @@ $(document).ready(function() {
     var answer = $(this).data('fields').replace(regexp, unique_id);
     $(this).closest('table').find('tr').last().before(answer);
   });
-  
 });
 
-//remove extra answers; we just need True and False
-function set_true_false_defaults() {
+//remove extra answers (or add, if necessary) so that we
+//just have 2 answers: True and False
+function setTrueFalseDefaults() {
     if ($(this).val() == "T/F") {
       var num_answers = $('.answer-row:visible').length;
-      
-      for(i = 1; i <= (num_answers - 2); i++) {   
+      for(i = 1; i <= (num_answers - 2); i++) {
         $('.answer-row:visible').last().
           find('.question_answers__destroy').
           find('input').val('1');
         $('.answer-row:visible').last().hide();
+      }
+      for(i = 1; i <= (2 - num_answers); i++) {
+          $('.add_fields').trigger('click');
       }
       $('.answer-row:visible').last().find('textarea').val("False");
       $('.answer-row:visible').first().find('textarea').val("True");
