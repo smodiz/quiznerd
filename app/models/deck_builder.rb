9345d65f09ab6ecb391af8c7ce1036b8ef955ddd
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-08 16:10:18 -0700

    Add model for creating a flash deck from a quiz

---
+++ app/models/deck_builder.rb
@@ -0,0 +1,45 @@
+class DeckBuilder
+
+  def self.from_quiz(quiz, difficulty=nil)
+    deck = Deck.new(  
+      name:         quiz.name, 
+      description:  quiz.description, 
+      status:       Deck::STATUSES[0],
+      user:         quiz.author, 
+      tag_list:     quiz.subject_name.downcase.gsub(" ", "-")
+    )
+    deck.flash_cards = self.flash_cards_from_questions(
+      deck.id, 
+      difficulty, 
+      quiz.questions
+    )
+    deck
+  end
+
+  private
+  
+  def self.flash_cards_from_questions(deck_id, difficulty, questions)
+    flash_cards = []
+    questions.each_with_index do |question, index|
+      flash_cards << FlashCard.new( deck_id: deck_id,
+                                    front: self.build_front(question), 
+                                    back: self.build_back(question.answers),
+                                    sequence: index + 1,
+                                    difficulty: difficulty)
+    end
+    flash_cards
+  end
+
+  def self.build_back(answers)
+    answers.each_with_object("") { |a, back| back << "#{a.content}\n" if a.correct? }
+  end 
+
+  def self.build_front(question)
+    if question.question_type == "T/F"
+      "True or False:\n\n#{question.content}"
+    else
+      question.content
+    end
+  end
+
+end
---
+++ spec/models/deck_builder_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe DeckBuilder do
+
+  describe "created from quiz" do
+    it "returns a deck with flash cards" do
+      default_difficulty = "2"
+      quiz = FactoryGirl.create(:quiz_with_questions)
+      # factory creates 2 questions, each with 2 answers, 
+      # one of which is correct. For a more complete test,
+      # make one of the questions have 2 correct answers
+      quiz.questions.first.answers[1].correct = true
+      deck = DeckBuilder.from_quiz(quiz, default_difficulty)
+      deck.save!
+
+      expect(deck.flash_cards.size).to eq quiz.questions.size
+      deck.flash_cards.each_with_index do |flash_card, index|
+        expect(question_equivalent?(  flash_card, 
+                                      quiz.questions[index],
+                                      default_difficulty)
+        ).to eq true
+      end
+    end
+  end
+
+  def question_equivalent?(flash_card, question, difficulty)
+    flash_card.front.include?(question.content) && 
+      answer_equivalent?(question.answers, flash_card) && 
+      flash_card.difficulty == difficulty
+  end
+
+  def answer_equivalent?(answers, flash_card)
+    answers.any? do |a| 
+      #xnor -> expect either true && true or false && false
+      !(flash_card.back.include?(a.content) ^ a.correct?)
+    end
+  end
+end
