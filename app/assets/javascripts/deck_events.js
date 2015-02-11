d37214ef1bc62a659631feead8b7eae2667142b3
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-11 11:53:19 -0700

    set focus on the start button on page load

--- app/assets/javascripts/deck_events.js
+++ app/assets/javascripts/deck_events.js
@@ -7,6 +7,10 @@ $(document).ready( function(){
   if (isFiltered()  && isValidCount()) {
     playFlashCards();
   }
+  else {
+    $('#new-deck-event-button').select();
+  }
+
 });
 
 // <!-- functions -->
