50086077151db8a6e70e6f7fc748ef0d857ab77e
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-11 22:49:23 -0700

    Small changes, set focus in appropriate field on page load, etc

--- app/assets/javascripts/flash_cards.js
+++ app/assets/javascripts/flash_cards.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+  
   $( "#sortable" ).sortable();
   $( "#sortable" ).disableSelection();
   
   $('.flash-cards-container').on('click', '#done', function(event) {
     event.preventDefault();
-    $('.flash-card-form').remove();
     $('#new_link').show();
+
   });
  
 });
