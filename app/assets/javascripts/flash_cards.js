530408a00c6e54abc4980ae3076afb2a175aef5f
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-11 23:40:52 -0700

    Hide form when hide is clicked

--- app/assets/javascripts/flash_cards.js
+++ app/assets/javascripts/flash_cards.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   
   $('.flash-cards-container').on('click', '#done', function(event) {
     event.preventDefault();
+    $('#new_flash_card').remove();
     $('#new_link').show();
-
   });
  
 });
