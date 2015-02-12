db2aab51a133bf4fed4a0533d08d52365f52662a
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-11 23:19:36 -0700

    Refactor javascript for deck event

--- app/assets/javascripts/deck_events.js
+++ app/assets/javascripts/deck_events.js
@@ -1,19 +1,19 @@
 
 $(document).ready( function(){
-  rememberOriginalSettings();
-  process.init();
-  process.advance();
-  process.answer();
+  
+  init();
+  showAnswer();
+  processAnswer();
+
   if (isFiltered()  && isValidCount()) {
     playFlashCards();
   }
   else {
     $('.new-deck-event-btn').focus();
   }
-
 });
 
-// <!-- functions -->
+// functions 
 
 function isValidCount() {
   return ($('#flash-card-set-count').text() > 0);
@@ -27,11 +27,6 @@ function rememberOriginalSettings() {
    elem.data('oldVal', elem.val());
   });
 }
-function playFlashCards() {
-  $('.options-section').hide();
-  $('.de-flash-card-side').first().show();
-  $('.de-flash-card-side').first().find('.flash-advance').focus();
-}
 function changesMadeToFilters() {
   changes = false;
   $('.deck-event-option').each(function() {
@@ -43,57 +38,69 @@ function changesMadeToFilters() {
   });
   return changes;
 }
-var process = {
-  init: function() {
-    $('.new-deck-event-btn').on('click', function(event){
-      if (!changesMadeToFilters() && isValidCount()) {
-        event.preventDefault();
-        playFlashCards();
-      }
-    });
-  },
-  advance: function() {
-    event.preventDefault();
-    $('.flash-advance').on('click', function(event){
-      $(this).closest('.de-flash-card-side').next().show();
-      $(this).closest('.de-flash-card-side').hide();
-      $(this).closest('.de-flash-card-side').next().find('.correct-flash-answer').focus();
-    });
-  },
-  answer: function() {
-    event.preventDefault();
-    $('.flash-answer').on('click', function(event){
+var init = function() {
+  $('.new-deck-event-btn').on('click', function(event){
+    if (!changesMadeToFilters() && isValidCount()) {
+      event.preventDefault();
+      rememberOriginalSettings();
+      playFlashCards();
+    }
+  });
+}
+function playFlashCards() {
+  $('.options-section').hide();
+  $('.de-flash-card-side').first().show();
+  $('.de-flash-card-side').first().find('.flash-advance').focus();
+}
+var processAnswer = function() {
+  $('.flash-answer').on('click', function(event){
 
-      //first, deal with grading
-      if ($(this).attr("class").match(/incorrect/)) {
-        var id = $(this).attr("id").split("-")[2];
-        var missed_cards_list = $('#deck_event_missed_cards_list').val();
-        if (missed_cards_list && missed_cards_list.length > 0) {
-          missed_cards_list += ","
-        }
-        missed_cards_list += id
-        $('#deck_event_missed_cards_list').val(missed_cards_list);
-      }
-      else {
-        var currentCount = parseInt($('#deck_event_total_correct').val());
-        $('#deck_event_total_correct').val(currentCount + 1);
-      }
-      //then, display the next one or the finished message  
-      $(this).closest('.de-flash-card-side').hide();
-      if($(this).closest('.de-flash-card-side').next()[0] == null) {
-        $('#de-finished').show();
-        $('.de-form-actions').show();
-        var correct = parseInt($('#deck_event_total_correct').val());
-        var total = parseInt($('#deck_event_total_cards').val());
-        var score = (correct/total * 100).toFixed(0);
-        $('#de-score').text("You scored " + score + "%!");
-         $(this).off('click'); 
-        $('#new_deck_event').find('.btn-primary').focus();
-      }
-      else {
-        $(this).closest('.de-flash-card-side').next().show();
-        $(this).closest('.de-flash-card-side').next().find('.flash-advance').focus();
-      }
-    });
+    //first, deal with grading
+    gradeAnswer(this);
+
+    //then, display the next one or the finished message  
+    $(this).closest('.de-flash-card-side').hide();
+    if($(this).closest('.de-flash-card-side').next()[0] == null) {
+      showScore();
+    }
+    else {
+      advanceToNext(this);
+    }
+  });
+}
+var gradeAnswer = function(elem) {
+  if ($(elem).attr("class").match(/incorrect/)) {
+    var id = $(elem).attr("id").split("-")[2];
+    var missed_cards_list = $('#deck_event_missed_cards_list').val();
+    if (missed_cards_list && missed_cards_list.length > 0) {
+      missed_cards_list += ","
+    }
+    missed_cards_list += id
+    $('#deck_event_missed_cards_list').val(missed_cards_list);
   }
+  else {
+    var currentCount = parseInt($('#deck_event_total_correct').val());
+    $('#deck_event_total_correct').val(currentCount + 1);
+  }
+}
+var advanceToNext = function(elem) {
+  $(elem).closest('.de-flash-card-side').next().show();
+  $(elem).closest('.de-flash-card-side').next().find('.flash-advance').focus();
+}
+var showScore = function() {
+  $('#de-finished').show();
+  $('.de-form-actions').show();
+  var correct = parseInt($('#deck_event_total_correct').val());
+  var total = parseInt($('#deck_event_total_cards').val());
+  var score = (correct/total * 100).toFixed(0);
+  $('#de-score').text("You scored " + score + "%!");
+  $(this).off('click'); 
+  $('#new_deck_event').find('.btn-primary').focus();
+}
+var showAnswer = function() {
+  $('.flash-advance').on('click', function(event){
+    $(this).closest('.de-flash-card-side').next().show();
+    $(this).closest('.de-flash-card-side').hide();
+    $(this).closest('.de-flash-card-side').next().find('.correct-flash-answer').focus();
+  });
 }
