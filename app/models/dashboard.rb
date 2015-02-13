fb890ad37bed1bdd1e75f4808c6d1511e0b2d696
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-12 22:25:01 -0700

    Add badges with total count for each type of thing, in each panel on the home page.

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,17 +1,33 @@
 class Dashboard
 
   attr_reader :user, :quizzes, :quiz_events, :limit
-  attr_reader :num_remaining_quizzes, :num_remaining_events
+  attr_reader :quizzes_count, :events_count
   attr_reader :cheatsheets, :decks
-  attr_reader :num_remaining_cheatsheets, :num_remaining_decks
+  attr_reader :cheatsheets_count, :decks_count
 
   def initialize(user, limit)
     @user = user
     @limit = limit
+    set_counts
     load
-   
   end
 
+  def remaining_quizzes_count
+    quizzes_count - quizzes.size
+  end
+  
+  def remaining_events_count
+    events_count - quiz_events.size
+  end
+  
+  def remaining_cheatsheets_count
+    cheatsheets_count - cheatsheets.size
+  end
+  
+  def remaining_decks_count
+    decks_count - decks.size
+  end
+  
   private
 
   def load
@@ -23,25 +39,32 @@ class Dashboard
 
   def load_quizzes
     user_quizzes = Quiz.cached_for_user(user)
+    @quizzes_count = user_quizzes.size
     @quizzes = user_quizzes.slice(0, limit)
-    @num_remaining_quizzes = user_quizzes.size - quizzes.size
   end
 
   def load_quiz_events
     user_events = QuizEvent.cached_for_user(user)
+    @events_count = user_events.size
     @quiz_events = user_events.slice(0, limit)
-    @num_remaining_events = user_events.size - quiz_events.size
   end
 
   def load_cheatsheets
     user_cheatsheets = Cheatsheet.cached_for_user(user)
+    @cheatsheets_count = user_cheatsheets.size
     @cheatsheets = user_cheatsheets.slice(0, limit)
-    @num_remaining_cheatsheets = user_cheatsheets.size - cheatsheets.size
   end
 
   def load_flash_card_decks
     user_decks = Deck.cached_for_user(user)
+    @decks_count = user_decks.size
     @decks = user_decks.slice(0, limit)
-    @num_remaining_decks = user_decks.size - decks.size
+  end
+
+  def set_counts
+    @quizzes_count = 0 
+    @events_count = 0
+    @cheatsheets_count = 0 
+    @decks_count = 0
   end
 end
