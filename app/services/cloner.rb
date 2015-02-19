25528733f73466b7d9e291f1e4c6b3cf186c23db
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-19 15:42:01 -0700

    Change the 'copy question' code to use the 'dup' method in Active Record.

--- app/services/cloner.rb
+++
@@ -1,7 +0,0 @@
-class Cloner 
-
-  def self.clone(obj)
-    new_obj = obj.class.new.from_xml(obj.to_xml except: [ :id, :created_at, :updated_at])
-  end
-
-end
--- app/services/question_cloner.rb
+++ app/services/question_cloner.rb
@@ -1,12 +1,11 @@
 class QuestionCloner
 
   def self.clone(question)
-    new_question = Cloner.clone(question)
+    new_question = question.dup
     question.answers.each do |answer|
-      new_question.answers << Cloner.clone(answer)
+      new_question.answers << answer.dup
     end
     new_question
-
   end
 
 end
