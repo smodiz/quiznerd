82a0b372101dcb2345675e0b45215aa8afd86e30
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-19 14:13:04 -0700

    Move additional view logic into the presenter.

--- app/presenters/grade_presenter.rb
+++ app/presenters/grade_presenter.rb
@@ -8,4 +8,9 @@ class GradePresenter
     "#{grade.percent}% (#{grade.correct} out of #{grade.answered})"
   end
 
+  def self.long_score_with_time(grade, time, view_context)
+    "You scored #{long_score(grade)} \  
+       #{view_context.time_ago_in_words(time)} ago!" 
+  end
+
 end
