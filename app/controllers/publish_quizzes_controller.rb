0d281c554a1d05fa40b1d3c10cf3cc63f9a7ee2f
Sharon Modiz <sharonmodiz@yahoo.com>
2015-02-22 23:01:19 -0700

    Add authorize user to quiz publishing

--- app/controllers/publish_quizzes_controller.rb
+++ app/controllers/publish_quizzes_controller.rb
@@ -1,13 +1,19 @@
 class PublishQuizzesController < ApplicationController
   
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :authorize_user
 
   def create
-    @quiz = Quiz.find(params[:quiz_id])
     @quiz.toggle_publish
     @quiz.save
     action = @quiz.published ? "published" : "unpublished" 
     redirect_to @quiz, notice: "Quiz was #{action}" 
+  end
+
+  private
+
+  def authorize_user
+    @quiz = current_user.quizzes.find_by(id: params[:quiz_id])
+    redirect_to root_url, notice: "You cannot publish that Quiz." if @quiz.nil?
   end
 
 end
