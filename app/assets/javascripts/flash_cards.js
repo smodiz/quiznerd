3fc2b34876aea1e4a8d19651f7a2d20d5466764c
Sharon Modiz <sharonmodiz@yahoo.com>
2015-04-01 10:29:33 -0600

    Remove the ability to re-sequence flash cards, since currently not saving re-sequenced rows. Will include in future enhancement

--- app/assets/javascripts/flash_cards.js
+++ app/assets/javascripts/flash_cards.js
@@ -1,12 +1,7 @@
-$(document).ready(function(){
-  
-  $( "#sortable" ).sortable();
-  $( "#sortable" ).disableSelection();
-  
+$(document).ready(function(){  
   $('.flash-cards-container').on('click', '#done', function(event) {
     event.preventDefault();
     $('form').remove();
     $('#new_link').show();
   });
- 
 });
