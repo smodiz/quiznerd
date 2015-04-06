0057d10f77cdae4dc1fb04d61c8c0fa471747e13
Sharon Modiz <sharonmodiz@yahoo.com>
2015-04-05 19:28:38 -0600

    Force ssl on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, :if => Proc.new { |c| c.request.format == 'application/html' }
+  force_ssl if Rails.env.production?
 end
