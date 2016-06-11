81eba844ff717d7ec17c54156ada058d69bba48e
Sharon Modiz <sharonmodiz@yahoo.com>
2016-06-10 18:08:57 -0600

    Refactor quiz_events_spec, plus move all helper methods that are related solely to this spec into this file instead of having them in utilities.rb

--- spec/requests/quiz_events_spec.rb
+++ spec/requests/quiz_events_spec.rb
@@ -1,55 +1,67 @@
 require 'spec_helper'
 
 describe 'Quiz Event Pages' do
-  let(:quiz_event)  { FactoryGirl.create(:small_quiz_event) }
-  let(:new_quiz_url)  { new_quiz_url_for(quiz_event.quiz.id) }
 
   before(:each) do
-    valid_sign_in(quiz_event.user)
+    valid_sign_in(user)
   end
 
   subject { page }
 
   describe 'shows a quiz event on the index page' do
-    before(:each) do
+    it 'displays the proper fields' do
+      quiz_event = FactoryGirl.create(:small_quiz_event, user: user)
+
       visit quiz_events_path
+
+      expect(page).to have_content('Quizzes Taken')
+      expect(page).to have_content(quiz_event.quiz.name)
+      expect(page).to have_content(quiz_event.total_correct)
+      expect(page).to have_content(quiz_event.total_answered)
     end
 
-    it { should have_content('Quizzes Taken') }
-    it { should have_content(quiz_event.quiz.name) }
-    it { should have_content(quiz_event.total_correct) }
-    it { should have_content(quiz_event.total_answered) }
-  end
+    it 'does not show other users quiz events' do
+      other_user_quiz_event = FactoryGirl.create(:small_quiz_event)
 
-  describe 'does not show other users quiz event' do
-    let(:quiz_event_2)  { FactoryGirl.create(:small_quiz_event) }
-    before(:each) do
       visit quiz_events_path
+
+      expect(page).not_to have_content(other_user_quiz_event.quiz.name)
     end
-    it { should_not have_content(quiz_event_2.quiz.name) }
   end
 
   describe 'shows quiz detail on the create page' do
-    before { visit new_quiz_url }
-    it { should have_content(quiz_event.quiz.name) }
-    it { should have_link('Take This Quiz') }
+    it 'displays the proper fields' do
+      quiz = FactoryGirl.create(:quiz_with_questions)
+
+      visit new_quiz_url_for(quiz.id)
+
+      expect(page).to have_content(quiz.name)
+      expect(page).to have_link('Take This Quiz')
+    end
   end
 
-  describe 'taking the quiz' do
-    before(:each) do
-      visit new_quiz_url
-      click_link 'Take This Quiz'
-    end
+  describe 'when taking the quiz (creating a new quiz event)' do
+    # before(:each) do
+    #   @quiz = FactoryGirl.create(:quiz_with_questions)
+    #   visit new_quiz_url_for(@quiz.id)
+    #   click_link 'Take This Quiz'
+    # end
 
     describe 'when I start the quiz' do
-      it { should have_content('Question 1') }
-      it { should have_button('Continue') }
-      it { should have_link('Quit') }
+      it 'displays the proper fields on the start page' do
+        start_quiz
+        expect(page).to have_content('Question 1')
+        expect(page).to have_button('Continue')
+        expect(page).to have_link('Quit')
+      end
 
       context 'author deletes the quiz after I start taking it' do
         it 'redirects me to the home page with a message' do
-          quiz_event.quiz.destroy!
+          quiz = start_quiz
+          quiz.destroy!
+
           click_button 'Continue'
+
           expect(current_path).to eq(root_path)
           expect(page).to have_content 'author must have deleted that quiz'
         end
@@ -57,44 +69,47 @@ describe 'Quiz Event Pages' do
     end
 
     describe 'when I answer the question', js: true do
-      let(:question_type) do
-        quiz_event.quiz.questions.first.question_type
-      end
-      let(:incorrect_answer_text) do
-        incorrect_answer(quiz_event.quiz.questions.first)
-      end
-      let(:correct_answer_text) do
-        correct_answer(quiz_event.quiz.questions.first)
-      end
+      # let(:question) do
+      #   @quiz.questions.first
+      # end
 
       it 'should require an answer to be selected' do
+        start_quiz
         click_button 'Continue'
         expect(page).to have_content('Answer ids can\'t be blank')
       end
 
       context 'correctly' do
         it 'should display message for correctly answered question' do
-          answer_correctly
+          quiz = start_quiz
+
+          answer_correctly(quiz.questions.first)
           click_button 'Continue'
+
           expect(page).to have_content 'Correct!'
         end
       end
 
       context 'incorrectly' do
-        before(:each) do
-          answer_incorrectly
-          click_button 'Continue'
-        end
-
         it 'should display message for incorrectly answered question' do
+          quiz = start_quiz
+
+          answer_incorrectly(quiz.questions.first)
+          click_button 'Continue'
+
           expect(page).to have_content 'Incorrect!'
         end
 
         it 'should not allow user to use back button to re-answer' do
-          expect(page).to have_content 'Incorrect!'
-          page.evaluate_script('window.history.back()')
-          answer_correctly
+          quiz = start_quiz
+
+          answer_incorrectly(quiz.questions.first)
           click_button 'Continue'
+
+          page.evaluate_script('window.history.back()')
+          answer_correctly(quiz.questions.first)
+          click_button 'Continue'
+
           expect(page).to have_content 'Correct'
         end
       end
@@ -103,13 +118,7 @@ describe 'Quiz Event Pages' do
 
   describe 'when I finish the quiz', js: true do
     before do
-      visit new_quiz_url
-      click_link 'Take This Quiz'
-      quiz_event.quiz.questions.each do |question|
-        answer_text = question.answers.first.content
-        check(answer_text)
-        click_button 'Continue'
-      end
+      take_quiz
     end
 
     it 'should display completion message' do
@@ -125,59 +134,129 @@ describe 'Quiz Event Pages' do
 
   describe 'Show Quiz Event' do
     context 'when quiz still exists and is public' do
-      before do
+      it 'shows the "Take Again" link' do
+        quiz_event = FactoryGirl.create(:small_quiz_event, user: user)
         visit quiz_events_path
-        click_link quiz_event.quiz.name
-      end
 
-      it { should have_content(quiz_event.quiz.name) }
-      it { should have_content quiz_event.status }
-      it { should have_link 'Edit' }
-      it 'should have take again link' do
+        click_link quiz_event.quiz.name
+
+        expect(page).to have_content(quiz_event.quiz.name)
+        expect(page).to have_content quiz_event.status
+        expect(page).to have_link 'Edit'
         expect(page).to have_link 'Take This Quiz Again'
       end
     end
 
     context 'after display show page but before clicking Take This Quiz link' do
       it 'routes me to the index page if the author deletes the quiz' do
+        quiz_event = FactoryGirl.create(:small_quiz_event, user: user)
         visit new_quiz_event_path(quiz_id: quiz_event.quiz.id)
         quiz_event.quiz.destroy!
+
         click_link 'Take This Quiz'
+
         expect(current_path).to eq(root_path)
       end
     end
 
     context 'when quiz still exists but is not public any more' do
-      before do
-        quiz_event.quiz.toggle_publish
-        quiz_event.quiz.save!
-        visit quiz_events_path
-        click_link quiz_event.quiz.name
-      end
+      it 'does not show the "Take Again" link' do
+        @quiz_event = FactoryGirl.create(:small_quiz_event, user: user)
+        @quiz_event.quiz.toggle_publish
+        @quiz_event.quiz.save!
 
-      it { should have_content(quiz_event.quiz.name) }
-      it { should have_content quiz_event.status }
-      it { should have_link 'Edit' }
-      it 'should not have take again link' do
+        visit quiz_events_path
+        click_link @quiz_event.quiz.name
+
+        expect(page).to have_content(@quiz_event.quiz.name)
+        expect(page).to have_content @quiz_event.status
+        expect(page).to have_link 'Edit'
         expect(page).not_to have_link 'Take This Quiz Again'
       end
     end
   end
 
   describe 'when user quits' do
-    before do
-      visit new_quiz_url
+    it 'should redirect the user to the home page' do
+      quiz = FactoryGirl.create(:quiz_with_questions)
+
+      visit new_quiz_url_for(quiz.id)
       click_link 'Take This Quiz'
       click_link 'Quit'
-    end
 
-    it 'should redirect the user to the home page' do
       expect(current_path).to eq(root_path)
-    end
-    it 'should remove the quiz_event from the database' do
       within('#quizzes') do
-        expect(page).to_not have_link quiz_event.quiz.name
+        expect(page).to_not have_link quiz.name
       end
     end
+  end
+end
+
+# ---------------  helper methods --------------
+
+def user
+  @user ||= FactoryGirl.create(:user)
+end
+
+def answer_incorrectly(question)
+  incorrect_answer_text = incorrect_answer(question)
+  if question.question_type == 'MC-2'
+    check(incorrect_answer_text) # checkbox
+  else
+    choose(incorrect_answer_text) # radio button
+  end
+end
+
+def answer_correctly(question)
+  correct_answer_text = correct_answer(question)
+  if question.question_type == 'MC-2'
+    correct_answer_text.each do |answer_text|
+      check(answer_text) # checkbox
+    end
+  else
+    choose(correct_answer_text.to_s) # radio button
+  end
+end
+
+def correct_answer(question)
+  # need ALL the correct answers
+  answer_texts(question.correct_answer_ids)
+end
+
+def incorrect_answer(question)
+  # just need ONE incorrect answer
+  incorrect_id = question.incorrect_answer_ids[0]
+  Answer.find(incorrect_id).content
+end
+
+def answer_texts(answer_ids)
+  answer_texts = []
+  answer_ids.each do |answer_id|
+    answer_texts << Answer.find(answer_id).content
+  end
+  answer_texts
+end
+
+def new_quiz_url_for(quiz_id)
+  "/quiz_events/new?quiz_id=#{quiz_id}"
+end
+
+def start_quiz
+  quiz = FactoryGirl.create(:quiz_with_questions)
+  visit new_quiz_url_for(quiz.id)
+  click_link 'Take This Quiz'
+  quiz
+end
+
+def take_quiz
+  quiz = FactoryGirl.create(:quiz_with_questions)
+  new_quiz_url = new_quiz_url_for(quiz.id)
+
+  visit new_quiz_url
+  click_link 'Take This Quiz'
+  quiz.questions.each do |question|
+    answer_text = question.answers.first.content
+    check(answer_text)
+    click_button 'Continue'
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,50 +7,6 @@ def valid_sign_in(user)
   click_button 'Sign in'
 end
 
-# Note: These methds cannot be named the same as the variable specified in the
-# let methods that use them. See quiz_events_spec.rb
-
-def correct_answer(question)
-  # need all the correct answers
-  answer_texts(question.correct_answer_ids)
-end
-
-def incorrect_answer(question)
-  # just need one incorrect answer
-  incorrect_id = question.incorrect_answer_ids[0]
-  Answer.find(incorrect_id).content
-end
-
-def answer_texts(answer_ids)
-  answer_texts = []
-  answer_ids.each do |answer_id|
-    answer_texts << Answer.find(answer_id).content
-  end
-  answer_texts
-end
-
-def answer_incorrectly
-  if question_type == 'MC-2'
-    check(incorrect_answer_text)
-  else
-    choose(incorrect_answer_text)
-  end
-end
-
-def answer_correctly
-  if question_type == 'MC-2'  # multiple answers, thus checkboxes
-    correct_answer_text.each do |answer_text|
-      check(answer_text)
-    end
-  else # single answer, thus radio button
-    choose(correct_answer_text.to_s)
-  end
-end
-
-def new_quiz_url_for(quiz_id)
-  "/quiz_events/new?quiz_id=#{quiz_id}"
-end
-
 # need this for those rare cases where I'm waiting
 # on a server side change via ajax that does not
 # change the dom on the browser, so can't just use
@@ -61,4 +17,3 @@ def wait_until
     loop until yield
   end
 end
-
